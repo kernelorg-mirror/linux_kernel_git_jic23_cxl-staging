d4285251d670f8cc43e14955eb8e2f1f6d74b447
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2024-05-10 17:52:47 +0100

    mm: memblock: Add a means to add to memblock.reserved
    For CXL CFMWS regions, need to add memblocks that may not be
    in the system memory map so that their nid can be queried later.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- mm/memblock.c
+++ mm/memblock.c
@@ -714,6 +714,17 @@ int __init_memblock memblock_add_node(phys_addr_t base, phys_addr_t size,
 	return memblock_add_range(&memblock.memory, base, size, nid, flags);
 }
 
+int __init_memblock memblock_add_reserved_node(phys_addr_t base, phys_addr_t size,
+				      int nid, enum memblock_flags flags)
+{
+	phys_addr_t end = base + size - 1;
+
+	memblock_dbg("%s: [%pa-%pa] nid=%d flags=%x %pS\n", __func__,
+		     &base, &end, nid, flags, (void *)_RET_IP_);
+
+	return memblock_add_range(&memblock.reserved, base, size, nid, flags);
+}
+
 /**
  * memblock_add - add new memblock region
  * @base: base address of the new region
